490047c4a937f520a8f31f80fc3a6efa15ad6b16
Angel Castillo <49908914+acastillorobles77@users.noreply.github.com>
2022-05-07 16:00:50 -0500

    Update lineage_vayu.mk

--- lineage_vayu.mk
+++ lineage_vayu.mk
@@ -26,7 +26,7 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="vayu_global-user 12 SKQ1.211006.001 V13.0.3.0.SJUMIXM release-keys" \
+    PRIVATE_BUILD_DESC="google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys" \
     PRODUCT_NAME=vayu_global \
     PRODUCT_MODEL=M2102J20SI
 
