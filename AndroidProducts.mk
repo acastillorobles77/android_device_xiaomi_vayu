feedca403be91906ca504abb1bc5ce747fa59a5b
Angel Castillo <49908914+acastillorobles77@users.noreply.github.com>
2022-06-23 16:43:31 -0500

    Update AndroidProducts.mk

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -8,6 +8,6 @@ PRODUCT_MAKEFILES := \
     $(LOCAL_DIR)/lineage_vayu.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_vayu-eng \
-    lineage_vayu-userdebug \
-    lineage_vayu-user
+    syberia_vayu-eng \
+    syberia_vayu-userdebug \
+    syberia_vayu-user
