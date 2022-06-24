96bea83b5d5f37bf60a4436114dc2f375ec38899
Angel Castillo <49908914+acastillorobles77@users.noreply.github.com>
2022-06-24 07:47:50 -0500

    Update syberia_vayu.mk

--- syberia_vayu.mk
+++ syberia_vayu.mk
@@ -14,6 +14,9 @@ $(call inherit-product, device/xiaomi/vayu/device.mk)
 # Inherit common ArrowOS configurations
 $(call inherit-product, vendor/syberia/config/common_full_phone.mk)
 
+# gapps
+$(call inherit-product-if-exists, vendor/gapps/basic/config.mk)
+
 # boot animation fix
 TARGET_BOOT_ANIMATION_RES := 1080
 
