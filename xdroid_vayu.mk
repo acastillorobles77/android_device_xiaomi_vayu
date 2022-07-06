b18103b0476ef6a8c189682a79734bee80acbd72
Angel Castillo <49908914+acastillorobles77@users.noreply.github.com>
2022-07-05 20:26:09 -0500

    Update and rename lineage_vayu.mk to xdroid_vayu.mk

--- xdroid_vayu.mk
+++ xdroid_vayu.mk
@@ -12,12 +12,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/vayu/device.mk)
 
 # Inherit common ArrowOS configurations
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/xdroid/config/common_full_phone.mk)
 
 # boot animation fix
 TARGET_BOOT_ANIMATION_RES := 1080
 
-PRODUCT_NAME := lineage_vayu
+PRODUCT_NAME := xdroid_vayu
 PRODUCT_DEVICE := vayu
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := Poco X3 Pro
