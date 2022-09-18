b9e32e03fbc8fc8aa08456f763166f55604d982f
Angel Castillo <acastillorobles77@gmail.com>
2022-09-18 16:36:50 -0500

    Improvements

--- aosp_vayu.mk
+++ aosp_vayu.mk
@@ -12,9 +12,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/vayu/device.mk)
 
 # Inherit common ArrowOS configurations
-$(call inherit-product, vendor/aosp/config/common.mk)
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-PRODUCT_NAME := aosp_vayu
+# Optionals
+TARGET_BOOT_ANIMATION_RES := 1080
+
+P6RODUCT_NAME := aosp_vayu
 PRODUCT_DEVICE := vayu
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := Poco X3 Pro
