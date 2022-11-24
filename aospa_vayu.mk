ae82573f46300c1fc7221e76dd0d52ba38bd8888
Angel Castillo <acastillorobles77@gmail.com>
2022-11-23 20:47:41 -0600

    vayu: Adapt device tree for AOSPA

--- aospa_vayu.mk
+++ aospa_vayu.mk
@@ -12,9 +12,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, device/xiaomi/vayu/device.mk)
 
 # Inherit common ArrowOS configurations
-$(call inherit-product, vendor/arrow/config/common.mk)
+$(call inherit-product, vendor/aospa/target/product/aospa-target.mk)
 
-PRODUCT_NAME := arrow_vayu
+# Optionals
+TARGET_BOOT_ANIMATION_RES := 1080
+
+PRODUCT_NAME := aospa_vayu
 PRODUCT_DEVICE := vayu
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := Poco X3 Pro
@@ -28,7 +31,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_MODEL=M2102J20SI
 
 BUILD_FINGERPRINT := Xiaomi/vayu_global/vayu:12/RKQ1.200826.002/V13.0.5.0.SJUMIXM:user/release-keys
-
-# ArrowOS additions
-DEVICE_MAINTAINER := kubersharma001, raghavt20
 
